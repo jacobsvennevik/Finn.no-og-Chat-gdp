c10b77cabb0dc87ccc84e4e36839ab09919c431d
jacobsvennevik <jacob.svennevik@gmail.com>
2023-01-17 17:50:57 +0100

    Ferdig justerad

--- Artikkel_Scraper.py
+++ Artikkel_Scraper.py
@@ -20,11 +20,18 @@ class Artikkel_Scraper:
         self.shadow = Shadow(self.driver)
         self.shadow.chrome_driver.get(url)
 
-    #finner nøkkelinfo
+    #SCRAPE fasiliteter balkong/tersasse osv
     def find_fas(self, tag, name):
-        tekst = []
+        tekst = ''
         for t in self.soup.find_all(tag, class_= name):
-            tekst.append(t.text)
+            tekst += f'{t.text}, '
+        return tekst
+
+      #Boks nede hvem passer nabolaget for?
+    def find_passer_for(self, name):
+        tekst = ''
+        for t in self.driver.find_elements(By.CLASS_NAME, name):
+            tekst += f'{t.text}, '
         return tekst
 
 
@@ -32,11 +39,20 @@ class Artikkel_Scraper:
     def find_el(self, tag, name):
         return self.soup.find(tag, class_= name).text
 
+
+
+    #finer teksen med nyLines
     def find_text_with_breaks(self, tag, name):
         soup = BeautifulSoup(str(self.soup).replace('<br/>', '\n'), 'html.parser')
-        return soup.find(tag, class_= name).text
+        return soup.find(tag, class_= name).text.replace('Om boligen', '')
 
-    #SCRAPE fasiliteter balkong/tersasse osv
+    def dict_to_string(self, dic):
+        str_info = ''
+        for key in dic:
+            str_info += f'{key} : {dic[key]}, '
+        return str_info
+
+    #finner nøkkelinfo
     def scrape_stats(self):
         stats_dict = dict()
         stats = self.soup.find("dl", class_="grid md:grid-cols-3 grid-cols-2 pb-8 gap-16 m-0")
@@ -46,7 +62,9 @@ class Artikkel_Scraper:
         for i in range(len(stats) - 1):
             stats_dict[stats_key[i].text] = stats_val[i].text
 
-        return stats_dict
+        return self.dict_to_string(stats_dict)
+
+
 
     #nærmeste kollektiv og skole
     def find_kollektiv_skole(self):
@@ -56,45 +74,17 @@ class Artikkel_Scraper:
         for i in range(len(ting)-1):
             tilgjengelighet_dict[ting[i].text] = tid[i].text
 
-        return tilgjengelighet_dict
+        return self.dict_to_string(tilgjengelighet_dict)
 
-    #Boks nede hvem passer nabolaget for?
-    def find_passer_for(self, name):
-        li = []
-        for t in self.driver.find_elements(By.CLASS_NAME, name):
-            li.append(t.text)
-        return li
+  
 
     #skraper ting som er i shadow
     def shadow_scrape(self, name):
-        li = []
+        tekst = ''
         elements = self.shadow.find_elements(name)
         for c in elements:
             c = c.text
-            if c not in li:
-                li.append(c)
-        return li
-
- 
-""" def run():
-
-    for url in read_urls(10):
-        scaper = Artikkel_Scraper(url)
-        print(scaper.find_el("div", "pt-16 sm:pt-40"))
-        print(scaper.find_el("span", "pl-4"))
-        print(scaper.find_fas("div", "py-4 break-words"))
-        print(scaper.scrape_stats())
-        print(scaper.find_kollektiv_skole())
-        print(scaper.find_passer_for("dp__cardContentListItem__3tbfT"))
-        print(scaper.shadow_scrape("h3[aria-label='Kontaktperson']"))
-
-    
-
-
- 
-run() """
-
-
-
-        
+            if c not in tekst:
+                tekst += f'{c}, '
+        return tekst
 
